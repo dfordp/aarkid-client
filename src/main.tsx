c235025b55e67f77e65372e495cf5e22140e4d78
Dilpreet <92905896+dfordp@users.noreply.github.com>
2025-11-10 14:24:15 +0530

    added analytics

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,14 @@ import './index.css'
 import { RecoilRoot } from 'recoil'
 import { CustomToaster } from './components/elements/Toaster.tsx'
 import DataLoader from './components/elements/DataLoader.tsx'
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RecoilRoot>
       <DataLoader/>
       <App />
+      <Analytics/>
       <CustomToaster/>
     </RecoilRoot>
   </React.StrictMode>,
